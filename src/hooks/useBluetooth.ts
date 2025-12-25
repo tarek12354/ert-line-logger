3692e737b90239aee9eba1322bb5ee9d16b14e6b
tarek12354 <34171892+tarek12354@users.noreply.github.com>
2025-12-25 20:47:45 +0100

    Update SERVICE_UUID to an empty string
    Changed SERVICE_UUID to an empty string.

--- src/hooks/useBluetooth.ts
+++ src/hooks/useBluetooth.ts
@@ -11,7 +11,7 @@ interface BluetoothState {
   permissionStatus: string | null;
 }
 
-const SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
+const SERVICE_UUID = '';
 const CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
 
 export const useBluetooth = () => {
